a8980cbf14f7e6633a3e261915239a289cc43cb4
willyc <willyc@supermicro.com.tw>
2022-07-08 14:18:03 +0800

    Add main code

--- StringConcatComparision/Program.cs
+++ StringConcatComparision/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StringConcatComparision
 {
     class Program
     {
+        private static readonly int ConcatCount = 300000;
+
         static void Main(string[] args)
         {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            //=================================================================//
+
+            //-------------------------
+            // Case 1: String
+            //-------------------------
+
+            var mode = "String";
+            var str = string.Empty;
+            for (int i = 0; i < ConcatCount; i++)
+            {
+                str += ".";
+            }
+
+            //-------------------------
+            // Case 2: StringBuilder
+            //-------------------------
+            
+            // var mode = "StringBuilder";
+            // var sb = new StringBuilder();
+            // for (int i = 0; i < ConcatCount; i++)
+            // {
+            //     sb.Append(".");
+            // }
+
+            //=================================================================//
+
+            stopWatch.Stop();
+            
+            var ts = stopWatch.Elapsed;
+
+            Console.WriteLine($"Mode: {mode}");
+            Console.WriteLine($"RunTime: {ts.TotalMilliseconds} ms");
         }
     }
 }
